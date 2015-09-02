54774482acd04f20a632d8b08ff702f3c2228b07
SvetaYurik <yurik.sveta@gmail.com>
2015-09-02 23:21:06 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main (String []args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println(Scanner.nextLine());
+}
+}
+
