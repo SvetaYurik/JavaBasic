831a538e964c70a43aa8cde37e90ef7a52f1e634
SvetaYurik <yurik.sveta@gmail.com>
2015-09-10 14:23:28 +0300

    not the last option

--- src/homeTask5/Main.java
+++ src/homeTask5/Main.java
@@ -3,17 +3,15 @@ package homeTask5;
 /**
  * Created by Света on 09.09.2015.
  */
-public class Main extends RectangleFigure{
+public class Main{
 
     public static void main (String[] args) {
 
         RectangleFigure rectangle = new RectangleFigure();
-        RectangleFigure obj = null;
-            obj.setA(12.0);
-            obj.setB(2.0);
-            obj.getA();
-            obj.getB();
-        double area = obj.getA() * obj.getB();
+
+            rectangle.setA(12.0);
+            rectangle.setB(2.0);
+            double area = rectangle.getA() * rectangle.getB();
         System.out.println("Area = " + area);
 
     }
